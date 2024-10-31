fa345667453279772e7607ea29d0860054342acc
kameta1008 <takemarue202@gmail.com>
2024-10-31 17:48:44 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :date_of_birth, presence: true
 end
