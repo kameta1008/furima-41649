ff9aa077e9ce1866719d8a4ead818930f947e4d6
kameta1008 <takemarue202@gmail.com>
2024-11-01 15:22:52 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
