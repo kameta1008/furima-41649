bc30981f47fccc6ebf24b29e573367f0c12e03d5
kameta1008 <takemarue202@gmail.com>
2024-11-04 17:41:25 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shopping_fee_id, :prefecture_id,
+                                 :scheduled_delivery_id, :price, :user, :image)
   end
 end
