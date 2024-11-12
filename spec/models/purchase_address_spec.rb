de0c7fbd2630af07e35d09c91f95162acb9506d8
kameta1008 <takemarue202@gmail.com>
2024-11-12 17:55:22 +0900

    商品購入機能修正②

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
   before do
-    @purchase_address = FactoryBot.build(:purchase_address, user_id: 1, item_id: 1)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item, user: user)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
